ebc24c27171798ac5965bd14ed4ea7643218aa11
Rafael <rapha.pse@outlook.com>
2017-07-02 21:26:57 -0300

    Formulário de cadastro e controle de uma loja

--- Loja/renderer.js
+++ Loja/renderer.js
@@ -1,11 +1,11 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+var loki = require('lokijs')
 var db = new loki('loki.json')
 var children = db.addCollection('children')
 clientes.insert({
     nome: 'Rafael Carvalho',
     email: 'rapha.pse@outlook.com'
-
 });
 db.save()
