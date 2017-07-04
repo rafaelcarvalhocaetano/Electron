7058b7abcbb9c1d0b988a8562889be32c35bf3c4
Rafael <rapha.pse@outlook.com>
2017-07-04 18:12:51 -0300

    Add Sistema de Loja

--- Loja/renderer.js
+++ Loja/renderer.js
@@ -1,9 +1,7 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-var loki = require('lokijs')
-var db = new loki('db.json')
-var clientes = db.addCollection('children')
+
 /*
 clientes.insert({
     nome: 'Rafael Carvalho',
