458fae16612d58652e2cb8185d5d54f66759f95f
Rafael <rapha.pse@outlook.com>
2017-07-17 21:23:01 -0300

    Adicionando alt

--- SIS-Cadastro/renderer.js
+++ SIS-Cadastro/renderer.js
@@ -1,4 +1,17 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+window.Vue = require('vue');
 
+new Vue({
+    el: 'body',
+    data: {
+    senha:false
+},
+  methods:{
+      editarVisitante:function(){
+
+      },
+
+  }
+})
