51905f202b415dfd2df7cdde13b644022f4db9d2
Rafael <rapha.pse@outlook.com>
2017-07-24 21:28:20 -0300

    Adicionando alterações no prototipo

--- form-db-mysql/renderer.js
+++ form-db-mysql/renderer.js
@@ -1,3 +1,66 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+/*
+<script>
+  //chama o mysql
+  //let mysql = require("mysql");
+  
+
+
+  //variavél que recebe a função de criar a base de dados
+  
+  //variavel que seleciona o elemento
+  /*
+    $query = 'SELECT * FROM `cadastro`';
+  //função que chama o elemento do banco de dados e mostra no html
+  conectar.query($query, (err, rows) => {
+    if(err){
+      throw err;
+    }
+    
+
+    for (i = 0; i < rows.length; i++) { 
+        $dados = rows[i];
+    } 
+    
+  });
+  */
+
+/*
+//inserindo dados com sucesso
+function enviar(){
+
+ var info = {
+   nome:$("#nome").val(),
+   empresa:$("#empresa").val(),
+   rg:$("#rg").val(),
+   data:$("#data").val()
+ };
+
+var query = conectar.query('insert into cadastro set ?', info, function(err, result){
+      if(err){
+        console.log(err);
+      }else{
+        console.log("secesso");
+      }
+      console.log(query.sql);
+ });
+conectar.end();
+}
+
+  let clientes = inserir;
+
+  new Vue({
+    el: 'body', //recebe o conteúdo do body
+      data: {
+         clientes:[],
+         mode:'',
+         client:{
+           nome:'',
+           cpf:'',
+           telefone:''
+         }
+})
+
+</script>*/
